ccc40815c8f30c9daa727ba7b1c24e01ca314ac1
Nur Ardhiansyah <ardhianzz@gmail.com>
2022-08-02 21:43:14 +0700

    menambahkan file Models dan Migrasi

--- database/migrations/2022_08_02_141228_create_pegawai_lampiran.php
+++ database/migrations/2022_08_02_141228_create_pegawai_lampiran.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePegawaiLampiran extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pegawai_lampiran', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('id_pegawai')->nullable();
+            $table->string('nama')->nullable();
+            $table->string('path')->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pegawai_lampiran');
     }
 }
