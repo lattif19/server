f97ab48c3128ff2bc94ff0cc202fa5c3dd55c068
Nur Ardhiansyah <ardhianzz@gmail.com>
2022-08-02 19:33:25 +0700

    menambahkan controller untuk modul dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AbsensiController;
 use App\Http\Controllers\AssetController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LemburController;
 use App\Http\Controllers\SppdController;
 use App\Http\Controllers\UserController;
@@ -24,10 +25,15 @@ Route::get('/', function () {
 
 
 Route::get('/pegawai', [UserController::class, 'index']);
-Route::get('/asset', [AssetController::class, 'index']);
+
+
+
+
+
+
+
 Route::get('/lembur', [LemburController::class, 'index']);
+Route::get('/asset', [AssetController::class, 'index']);
 Route::get('/sppd', [SppdController::class, 'index']);
 Route::get('/absen', [AbsensiController::class, 'index']);
-
-
-Route::get('/main', function(){ return view('dashboard/mainpage'); });
+Route::get('/main', [DashboardController::class, 'index']);
