c8e7a82d15ee7755369c279acb692a5828f506e9
Nur Ardhiansyah <ardhianzz@gmail.com>
2022-08-09 18:32:29 +0700

    Fixed Issue - Modal Pengaturan Mapping Absensi Tidak Tampil

--- resources/views/absen/pengaturan.blade.php
+++ resources/views/absen/pengaturan.blade.php
@@ -44,7 +44,7 @@
                                             <div class="modal-dialog" role="document">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
-                                                        <h5 class="modal-title" id="tambahData">Merubah Mapping Absensi</h5>
+                                                        <h5 class="modal-title" id="rubahData">Merubah Mapping Absensi</h5>
                                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                                 <span aria-hidden="true">&times;</span>
                                                              </button>
