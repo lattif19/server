aa7b33f2746147ef11cbdf0df060507f5b308630
Nur Ardhiansyah <ardhianzz@gmail.com>
2022-09-02 16:13:23 +0700

    menambah contoh pengaturan virtual host pada welcome-copy.blade.php

--- resources/views/welcome-copy.blade.php
+++ resources/views/welcome-copy.blade.php
@@ -1,1 +1,29 @@
 ini masternya merubah data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Contoh pengaturan virtual host windows untuk laravel 
+
+<VirtualHost 127.0.0.5:80>
+    DocumentRoot 'Folder Penyimpanan'
+    DirectoryIndex index.php
+    ServerName nama-web-server
+    <Directory 'Folder Penyimpanan'>
+    Options Indexes FollowSymLinks MultiViews
+    AllowOverride all
+    Order Deny,Allow
+    Allow from all
+    Require all granted
+    </Directory>
+</VirtualHost>
