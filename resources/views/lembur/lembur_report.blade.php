133a1514c0d5494fb21023042051407e45890d7a
lattif-mac <lattif-mac>
2022-09-06 13:50:32 +0700

    menambah fitur button hide/show filter di laporan lembur

--- resources/views/lembur/lembur_report.blade.php
+++ resources/views/lembur/lembur_report.blade.php
@@ -13,7 +13,7 @@
     {{ $filter          = request()->filter }}
 </div>
 <div class="container-fluid px-4">
-   <div hidden>
+   <div>
 
        <div class="row">
            <h1 class="mt-4">{{ $title }}</h1>
@@ -24,88 +24,98 @@
     </div>
     
     <div class="row">
-        <div class="col-lg-3">
-            <div class="card">
-                <div class="card-body">
-                    <form action="/lembur_report" method="get" class="form-inline">
-                        <div class="form-group mb-3">
-                            <label for="periode" class="mb-2"> <strong>Periode Lembur</strong></label>
-                            <select name="periode" class="form-control">
-                                @foreach ($periode as $p)
-                                    <option value="{{ $p->periode }}" @if($periode_hari_ini == $p->periode) selected @endif id="periode_lembur"> {{ $p->periode }}</option>
-                                @endforeach
-                            </select>
+        <div class="col-lg-12 ">
+            <div class="col">
+                <a href="#" class="btn btn-primary" data-toggle="collapse" id="hide_filter" aria-expanded="true">
+                    <i class="fa fa-filter"></i>
+                    <span class="hidden-xs" >Show Filter</span>
+                </a>
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-body">
+                            <form action="/lembur_report" method="get" id="show">
+                                <div class="form-group">
+                                    <label for="periode" class="mb-2"> <strong>Periode Lembur</strong></label>
+                                    <select name="periode" class="form-control">
+                                        @foreach ($periode as $p)
+                                            <option value="{{ $p->periode }}" @if($periode_hari_ini == $p->periode) selected @endif id="periode_lembur"> {{ $p->periode }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="form-group mt-3">
+                                    <div class="mb-2"><strong>Jenis Laporan</strong></div>
+                                    <div class="form-check form-check-inline">
+                                        <input  class="form-check-input" 
+                                                type="radio" 
+                                                name="jenis_report" 
+                                                id="inlineRadio1" 
+                                                value="general" 
+                                                @if($jenis_report == false || $jenis_report == "general") checked @endif>
+                                        <label class="form-check-label" for="inlineRadio1">General</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" 
+                                                type="radio" 
+                                                name="jenis_report" 
+                                                id="inlineRadio2" 
+                                                value="detail" 
+                                                @if($jenis_report == "detail") checked @endif>
+                                        <label class="form-check-label" for="inlineRadio2">Detail</label>
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group mt-3">
+                                    <div class="mb-2"><strong>Status</strong></div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" 
+                                                type="radio" 
+                                                name="filter" 
+                                                id="inlineRadio1" 
+                                                value="semua"  
+                                                @if($filter == false || $filter == "semua") checked @endif>
+                                        <label class="form-check-label" for="inlineRadio1">Semua</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" 
+                                                type="radio" 
+                                                name="filter" 
+                                                id="inlineRadio2" 
+                                                value="disetujui" 
+                                                @if($filter == "disetujui") checked @endif>
+                                        <label class="form-check-label" for="inlineRadio2">Disetujui</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" 
+                                                type="radio" 
+                                                name="filter" 
+                                                id="inlineRadio2" 
+                                                value="diajukan" 
+                                                @if($filter == "diajukan") checked @endif>
+                                        <label class="form-check-label" for="inlineRadio2">Diajukan</label>
+                                    </div>
+
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" 
+                                                type="radio" 
+                                                name="filter" 
+                                                id="inlineRadio3" 
+                                                value="selesai" 
+                                                @if($filter == "selesai") checked @endif>
+                                        <label class="form-check-label" for="inlineRadio3">Selesai</label>
+                                    </div>
+                                </div>
+
+                                <div class="form-group mt-3">
+                                    <button class="btn btn-warning text-light btn-block">
+                                        <i class="fa fa-check"></i>
+                                        Apply
+                                    </button>
+                                </div>
+                            </form>
                         </div>
-
-                        <div class="form-group mt-3">
-                            <div class="mb-2"><strong>Jenis Laporan</strong></div>
-                            <div class="form-check form-check-inline">
-                                <input  class="form-check-input" 
-                                        type="radio" 
-                                        name="jenis_report" 
-                                        id="inlineRadio1" 
-                                        value="general" 
-                                        @if($jenis_report == false || $jenis_report == "general") checked @endif>
-                                <label class="form-check-label" for="inlineRadio1">General</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" 
-                                        type="radio" 
-                                        name="jenis_report" 
-                                        id="inlineRadio2" 
-                                        value="detail" 
-                                        @if($jenis_report == "detail") checked @endif>
-                                <label class="form-check-label" for="inlineRadio2">Detail</label>
-                            </div>
-                        </div>
-
-
-                        <div class="form-group mt-3">
-                            <div class="mb-2"><strong>Filter</strong></div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" 
-                                        type="radio" 
-                                        name="filter" 
-                                        id="inlineRadio1" 
-                                        value="semua"  
-                                        @if($filter == false || $filter == "semua") checked @endif>
-                                <label class="form-check-label" for="inlineRadio1">Semua</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" 
-                                        type="radio" 
-                                        name="filter" 
-                                        id="inlineRadio2" 
-                                        value="disetujui" 
-                                        @if($filter == "disetujui") checked @endif>
-                                <label class="form-check-label" for="inlineRadio2">Disetujui</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" 
-                                        type="radio" 
-                                        name="filter" 
-                                        id="inlineRadio2" 
-                                        value="diajukan" 
-                                        @if($filter == "diajukan") checked @endif>
-                                <label class="form-check-label" for="inlineRadio2">Diajukan</label>
-                            </div>
-
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" 
-                                        type="radio" 
-                                        name="filter" 
-                                        id="inlineRadio3" 
-                                        value="selesai" 
-                                        @if($filter == "selesai") checked @endif>
-                                <label class="form-check-label" for="inlineRadio3">Selesai</label>
-                            </div>
-                        </div>
-
-                        <hr>
-                        <div class="form-group mt-3">
-                            <button class="btn btn-dark text-light btn-block">Tampilkan</button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -205,3 +215,14 @@ function ExportToExcel(type, fn, dl) {
 @endif
 @endsection
 
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+$("#show").hide();
+  $("#hide_filter").click(function(){
+    $("#show").show();
+  });
+});
+</script>
